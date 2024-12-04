238ab687d6dfdaa275bd51a744a3b6c8df71d802
JamezP2002 <117998954+JamezP2002@users.noreply.github.com>
2024-12-04 17:25:09 -0500

    updated sql file

--- sql/inital_sql_file.sql
+++ sql/inital_sql_file.sql
@@ -1,6 +1,6 @@
 use cdkey_db;
 
-DELETE TABLE users;
+DROP TABLE users;
 
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -13,3 +13,13 @@ INSERT INTO users (username, password) VALUES ('user', 'password');
 ALTER TABLE users ADD COLUMN role ENUM('buyer', 'seller') NOT NULL;
 
 select * from users;
+select * from games;
+
+CREATE TABLE games (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    cover_art VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    quantity INT NOT NULL,
+    cd_key TEXT NOT NULL
+);
